4441c8b931cbad555091f5921770a54939c8c39d
Gio <124griosramirez@frhsd.com>
2023-02-17 09:11:13 -0500

    even more snake code

--- js/snake.js
+++ js/snake.js
@@ -1,7 +1,9 @@
 import { getInputDirection } from "./input.js";
+const EXAPNSION_RATE = 1;
 
-export const SNAKE_SPEED = 1;
+export const SNAKE_SPEED = 5;
 const snakeBody = [{ x : 11, y : 11 }];
+let newSegments = 0;
 
 export function update() {
     const inputDirection = getInputDirection()
@@ -23,4 +25,18 @@ export function draw(gameBoard)
         snakeElement.classList.add('snake');
         gameBoard.appendChild(snakeElement);
     })
+}
+
+export function expandSnake(amount) {
+    newSegments += amount;
+}
+
+export function onSnake(position) {
+    return snakeBody.some(segment => {
+        return equalPositions(segment, position)
+    })
+}
+
+function equalPositions(pos1, pos2) {
+    return pos1.x === pos2.x && pos1.y === pos2.y;
 }
